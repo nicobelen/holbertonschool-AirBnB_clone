f27c3216733f1a2e53fb729c936b4cc32a816cdd
Vinill <danimuller758@gmail.com>
2022-06-28 14:31:51 +0000

    push 11:30

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 """promary class BaseModel"""
-
 from calendar import formatstring
 from locale import format_string
 import sys
@@ -12,11 +11,19 @@ from datetime import datetime
 class BaseModel():
     """instantiation of user"""
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
-    
     @classmethod
     def save(self):
         """atribute save"""
@@ -25,12 +32,10 @@ class BaseModel():
         
     def __str__(self):
         """atribute str"""
-        print("print de str")
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def to_dict(self):
         """returns a dictionary containing all keys and values"""
-        print("print de to_dict")
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         self.__dict__["__class__"] = self.__class__.__name__
