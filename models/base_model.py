3c4edc62df64e32f43980b7b14c6813e854fbebe
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-28 14:45:23 -0300

    pushing excercise five

--- models/base_model.py
+++ models/base_model.py
@@ -7,6 +7,7 @@ import sys
 from abc import ABC, abstractmethod
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel():
     """instantiation of user"""
@@ -23,10 +24,13 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
+            storage.new(self)
+
 
     @classmethod
     def save(self):
         """atribute save"""
+        storage.save()
         dateobj = datetime.utcnow()
         self.updated_at = dateobj.strftime("%Y-%m-%dT%H:%M:%S.%f")
         
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,30 @@
 """Serializes instances to a JSON file and deserializes JSON file to instances"""
 
 
-from base_model import BaseModel
+import json
 
 
-class FileStorage(BaseModel):
+class FileStorage():
     """initialization of FileStorage"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__file_path = 
+    __file_path = "file.json"
+    __objects = {}
+
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """writes an object to a textfile"""
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
+            return f.write(json.dumps(self.__objects, default = str))
+
+    def reload(self):
+        """creates an object from a “JSON file”"""
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as f:
+                self.__objects = json.loads(f.read())
+        except Exception:
+            pass
