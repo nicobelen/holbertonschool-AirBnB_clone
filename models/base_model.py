8ed21e70e4e8743767d080f012d6d6b95a81c920
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-28 10:08:36 -0300

    3 corregido

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,9 @@ from datetime import datetime
 class BaseModel():
     """instantiation of user"""
     def __init__(self, *args, **kwargs):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     
     @classmethod
@@ -31,4 +31,7 @@ class BaseModel():
     def to_dict(self):
         """returns a dictionary containing all keys and values"""
         print("print de to_dict")
-        return{"id": {self.id}, "created_at": {self.created_at}, "updated_at": {self.updated_at}, "__class__": self.__class__.__name__}
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__["__class__"] = self.__class__.__name__
+        return (self.__dict__)
