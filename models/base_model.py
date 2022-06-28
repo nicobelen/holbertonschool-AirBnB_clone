fd2d8119cf1070d8be8f4d65d46bb533761f6397
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-06-28 09:29:27 -0300

    morning push

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 
 """promary class BaseModel"""
 
+from calendar import formatstring
+from locale import format_string
 import sys
 from abc import ABC, abstractmethod
 import uuid
@@ -11,19 +13,22 @@ class BaseModel():
     """instantiation of user"""
     def __init__(self, *args, **kwargs):
         self.id = uuid.uuid4()
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
     
     @classmethod
     def save(self):
         """atribute save"""
-        self.updated_at = datetime.utcnow()
+        dateobj = datetime.utcnow()
+        self.updated_at = dateobj.strftime("%Y-%m-%dT%H:%M:%S.%f")
         
     def __str__(self):
         """atribute str"""
+        print("print de str")
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def to_dict(self):
         """returns a dictionary containing all keys and values"""
+        print("print de to_dict")
         return{"id": {self.id}, "created_at": {self.created_at}, "updated_at": {self.updated_at}, "__class__": self.__class__.__name__}
