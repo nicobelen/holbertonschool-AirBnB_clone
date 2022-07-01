f98f9aeb0b1d7209c2c1288b90953c6f60342091
Vinill <danimuller758@gmail.com>
2022-07-01 12:12:52 +0000

    pusheo perri

--- console.py
+++ console.py
@@ -9,7 +9,12 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 from models.state import State
+from models import storage
 import cmd
+import sys
+
+clases = {"State": State, "User": User, "Place": Place, "Review": Review,
+          "Amenity": Amenity, "City": City, "BaseModel": BaseModel}
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
@@ -22,16 +27,51 @@ class HBNBCommand(cmd.Cmd):
         "comand eof"
         return True
 
-    def do_create(self, line)
+    def do_create(self, line):
         arguments = line.split()
         if arguments == []:
             print("** class name missing **")
-        try:
-            
+            return
+        elif arguments[0] in clases:
+            instance = getattr(sys.modules[__name__], arguments[0])
+            inst = instance()
+            print(inst.id)
+        else:
+            print("** class doesn't exist **")
 
-    def do_show(self, line)
+    def do_show(self, line):
+        arguments = line.split()
+        if arguments == []:
+            print("** class name missing **")
+            return
+        if arguments[0] not in clases:
+            print("** class doesn't exist **")
+            return
+        if len(arguments) == 1:
+            print("** instance id missing **")
+            return
+        dic = storage.all()
+        key = f"{arguments[0]}.{arguments[1]}"
+        if key in dic:
+            print(dic[key])
+            return
 
-    def to_destroy(self, line)
-    
+    def do_destroy(self, line):
+        arguments = line.split()
+        if arguments == []:
+            print("** class name missing **")
+            return
+        if arguments[0] not in clases:
+            print("** class doesn't exist **")
+            return
+        if len(arguments) == 1:
+            print("** instance id missing **")
+            return
+        dic = storage.all()
+        key = f"{arguments[0]}.{arguments[1]}"
+        if key in dic:
+            del(dic[key])
+            return         
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
