a010e9f8e1d61a1ac82dc695a8c125dc7002d31c
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-03 14:49:04 -0300

    erasing imports on filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,14 +4,6 @@ deserializes JSON file to instances"""
 
 
 import json
-from os.path import exists
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage():
