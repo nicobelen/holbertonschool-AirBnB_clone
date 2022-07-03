183a7c37af40c76767aab5a608719c3946b8ec5e
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-07-03 19:34:49 -0300

    last try importing on filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,14 @@ deserializes JSON file to instances"""
 
 
 import json
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
